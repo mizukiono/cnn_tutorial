c9849e81691d33bd25fb3b0a8bcb1ea7266478da
Mizuki Ono <mizuki.ono@keio.jp>
2018-06-29 21:19:33 +0900

    specific image testing

--- butterflyfish/bfish_simplecnn.py
+++ butterflyfish/bfish_simplecnn.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[18]:
+# In[1]:
 
 
 from keras.models import Sequential
@@ -12,20 +12,20 @@ from keras.callbacks import EarlyStopping
 from keras.preprocessing.image import load_img, img_to_array
 
 
-# In[37]:
+# In[2]:
 
 
 import matplotlib.pyplot as plt
 import numpy as np
 
 
-# In[2]:
-
-
-epochs = 100
-
-
 # In[3]:
+
+
+epochs = 30
+
+
+# In[4]:
 
 
 # データをつくる
@@ -34,21 +34,21 @@ train_datagen = ImageDataGenerator(rescale=1.0/255, rotation_range=90, width_shi
 test_datagen = ImageDataGenerator(rescale=1.0/255)
 
 
-# In[4]:
+# In[5]:
 
 
 train_generator = train_datagen.flow_from_directory('../data/butterflyfish/train', target_size=(150,150),
                                                     batch_size=32, class_mode='binary')
 
 
-# In[5]:
+# In[6]:
 
 
 validation_generator = test_datagen.flow_from_directory('../data/butterflyfish/validation', target_size=(150,150),
                                                         batch_size=32, class_mode='binary')
 
 
-# In[6]:
+# In[7]:
 
 
 # モデルをつくる
@@ -66,31 +66,31 @@ model.add(Dropout(0.5))
 model.add(Dense(1, activation='sigmoid'))
 
 
-# In[42]:
+# In[8]:
 
 
 print(train_generator.class_indices)
 
 
-# In[7]:
+# In[9]:
 
 
 model.summary()
 
 
-# In[8]:
+# In[10]:
 
 
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 
 
-# In[9]:
+# In[11]:
 
 
 history = model.fit_generator(train_generator, epochs=epochs, verbose=1, validation_data=validation_generator)
 
 
-# In[10]:
+# In[12]:
 
 
 plt.plot(range(1, epochs+1), history.history['acc'], label="training")
@@ -101,22 +101,65 @@ plt.legend()
 plt.show()
 
 
-# In[38]:
+# In[27]:
 
 
 img = load_img("../data/originaldata/test.jpg", target_size=(150, 150))
 plt.imshow(img)
 
 
-# In[40]:
+# In[31]:
 
 
 array = img_to_array(img)
+array /= 255
 x = np.expand_dims(array, axis=0)
+pred = model.predict(x)
+print(pred[0])
+if pred[0] >= 0.5:
+    print('トゲチョウチョウウオです')
+else:
+    print('チョウチョウウオです')
 
 
-# In[41]:
+# In[35]:
 
 
-model.predict(x)
+img = load_img("../data/originaldata/test2.jpg", target_size=(150, 150))
+plt.imshow(img)
+
+
+# In[37]:
+
+
+array = img_to_array(img)
+array /= 255
+x = np.expand_dims(array, axis=0)
+pred = model.predict(x)
+print(pred[0])
+if pred[0] >= 0.5:
+    print('トゲチョウチョウウオです')
+else:
+    print('チョウチョウウオです')
+
+
+# In[38]:
+
+
+img = load_img("../data/originaldata/test3.jpg", target_size=(150, 150))
+plt.imshow(img)
+
+
+# In[39]:
+
+
+array = img_to_array(img)
+array /= 255
+x = np.expand_dims(array, axis=0)
+pred = model.predict(x)
+print(pred[0])
+if pred[0] >= 0.5:
+    print('トゲチョウチョウウオです')
+else:
+    print('チョウチョウウオです')
 
